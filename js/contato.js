fbdab5965346ba77608644ea95e4d9d839c2d852
MarihTavares <marina.ar1308@gmail.com>
2025-05-07 21:28:59 -0300

    fale conosco

---
+++ js/contato.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector("#enviar");
+let nome = document.querySelector("#nome");
+let email = document.querySelector("#titulo");
+let titulo = document.querySelector("#email");
+let mensagem = document.querySelector("#mensagem");
+
+botao.addEventListener('click',function(){
+    // alert('Sua mensagem foi enviada!!');
+})
