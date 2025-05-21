ceae3d954b4086fd4ad953955284eef57103621f
MarihTavares <marina.ar1308@gmail.com>
2025-05-21 19:43:52 -0300

    validação nome

--- js/contato.js
+++ js/contato.js
@@ -4,6 +4,18 @@ let nomeInput = document.querySelector("#nome");
 let emailInput = document.querySelector("#email");
 let tituloInput = document.querySelector("#titulo");
 let mensagemInput = document.querySelector("#mensagem");
+let erro = document.querySelector("#erro_nome");
+
+nomeInput.addEventListener('input',function(){
+    const palavras = nomeInput.value.trim().split(/\s+/); // trim - tira espaço antes e depois / split - palavras em array
+
+    if (palavras.length < 2){
+        erro.textContent = "Digite seu nome completo!!!";
+    }else {
+        erro.textContent = '';
+    }
+
+});
 
 //Função que é ativada a partir do click do botão
 botao.addEventListener('click',function(){
@@ -12,17 +24,26 @@ botao.addEventListener('click',function(){
     let nome = nomeInput.value;
     let email = emailInput.value;
     let titulo = tituloInput.value;
-    let mensage = mensagemInput.value;
+    let mensagem = mensagemInput.value;
+
+    const palavras = nomeInput.value.trim().split(/\s+/);
+    console.log(palavras)
 
     if (nome === "" || email === "" || titulo === "" || mensagem === ""){
-        alert("Preencha todas as informações!!");
-    }else{
-        alert("Enviado com sucesso!!");
 
-        //Limpa os campos
-        nomeInput.value = ""
-        emailInput.value = ""
-        tituloInput.value = ""
-        mensagemInput.value = ""
-    }
+        alert("Preencha todas as informações!!");  
+
+    }else if(palavras.length < 2) {
+
+        alert("Preencha seu nome completo!!");  
+
+    }else{
+         alert("Enviado com sucesso!!");
+        
+            //Limpa os campos
+            nomeInput.value = ""
+            emailInput.value = ""
+            tituloInput.value = ""
+            mensagemInput.value = ""
+        }
 })
